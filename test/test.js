77bc003a6c08c2d4b1155816c8afac039689e8b3
James Logan <jamesblogan20@gmail.com>
2015-07-22 14:21:59 -0500

    switch testing over to codeclimate

--- test/test.js
+++ test/test.js
@@ -74,66 +74,66 @@ describe('CLI', function () {
 //UNIT TESTS -----============---------==========
 
 
-// describe('Animal', function () {
-//   describe('constructor', function () {
-//     it('should return an animal object', function () {
-//       var animal = new Animal();
+describe('Animal', function () {
+  describe('constructor', function () {
+    it('should return an animal object', function () {
+      var animal = new Animal();
 
-//       animal.should.be.an('object');
-//       animal.should.be.an.instanceOf(Animal)
-//     })
+      animal.should.be.an('object');
+      animal.should.be.an.instanceOf(Animal)
+    })
 
-//     it('should be alive', function () {
-//       var animal = new Animal();
-//       animal.alive.should.be.true;
-//     })
-//     it('should have 100% health', function () {
-//       var animal = new Animal()
-//       animal.health.should.equal(1);
-//     })
-//     it('should accept a type', function () {
-//       var cat = new Animal('cat');
-//       var dog = new Animal('dog');
+    it('should be alive', function () {
+      var animal = new Animal();
+      animal.alive.should.be.true;
+    })
+    it('should have 100% health', function () {
+      var animal = new Animal()
+      animal.health.should.equal(1);
+    })
+    it('should accept a type', function () {
+      var cat = new Animal('cat');
+      var dog = new Animal('dog');
 
-//       cat.species.should.equal('cat');
-//       dog.species.should.equal('dog');
-//     });
+      cat.species.should.equal('cat');
+      dog.species.should.equal('dog');
+    });
 
-//     describe('#updateHealthStats()', function () {
-//       it('should change the health', function (done) {
-//         var animal = new Animal();
-//         var health = animal.health;
+    describe('#updateHealthStats()', function () {
+      it('should change the health', function (done) {
+        var animal = new Animal();
+        var health = animal.health;
 
-//         animal.updateHealthStats(function () {
-//           animal.health.should.not.equal(1);
-//           done();
-//         });
+        animal.updateHealthStats(function () {
+          animal.health.should.not.equal(1);
+          done();
+        });
 
-//       })
-
-
-//     })
-
-//     describe('#beCute()', function () {
-//       it('should be a prototype method', function () {
-//         var animal = new Animal();
-//         animal.should.respondTo('beCute');
-//         animal.should.not.have.ownProperty('beCute');
-//       })
-//       it('should make the animal cute', function () {
-//         var animal = new Animal();
-//         animal.isCute.should.not.be.true;
-//         animal.beCute();
-//         animal.isCute.should.be.true;
-//       })
-//     })
-
-//   });
+      })
 
 
+    })
+
+    describe('#beCute()', function () {
+      it('should be a prototype method', function () {
+        var animal = new Animal();
+        animal.should.respondTo('beCute');
+        animal.should.not.have.ownProperty('beCute');
+      })
+      it('should make the animal cute', function () {
+        var animal = new Animal();
+        animal.isCute.should.not.be.true;
+        animal.beCute();
+        animal.isCute.should.be.true;
+      })
+    })
+
+  });
 
 
-// });
+
+
+});
 
 describe('Tests', function() {
   it('truthyness', function () {
