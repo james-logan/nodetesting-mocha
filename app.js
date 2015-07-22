49eb3804a5970540dd288a708019ac0a0ac5a5b8
James Logan <jamesblogan20@gmail.com>
2015-07-22 09:40:59 -0500

    finished the cheer function added a travis.yml

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 #!/usr/bin/env node
+var cheer = require('./lib/name.js')
 
 
-console.log('Thanks for downloading my app!')
+var name = process.argv[2]
+
+process.stdout.write(cheer.cheerYeller(cheer.spaceBreaker(name)))
+
+
+
+
+
+// console.log('Give me a ....!')
--- test/test.js
+++ test/test.js
@@ -2,19 +2,71 @@ var should = require('chai').should();
 // var assert = require('assert')
 var Animal = require(process.cwd() + '/lib/Animal.js');
 var cp = require('child_process');
+var name = require(process.cwd() + '/lib/name.js')
 
-//INTEGRATION TESTS ---------------
-describe.only('CLI', function () {
-  it('should thank me for downloading', function (done) {
-    console.log('childprocess');
-    cp.execFile('./app.js', function (err, stdout) {
-      console.log('err', err);
-      console.log('stdout', stdout);
-      stdout.should.equal('Thanks for downloading my app!\n')
+
+
+//tests for Cheer.js -----------------
+// describe('CLI', function() {
+//   it('should print the first half of the sentence', function (done) {
+//     cp.execFile('./app.js', function (err, stdout) {
+//       stdout.should.equal('Give me a ....!\n');
+//       done();
+//     })
+//   });
+// })
+
+
+describe('String management', function () {
+  it('should remove spaces from the string', function () {
+    name.spaceBreaker('Mary Beth').should.eql(['M','A', 'R', 'Y', 'B', 'E', 'T', 'H'])
+  })
+})
+
+describe('Printind from the array', function () {
+  it('should print out messages for each letter', function () {
+    name.cheerYeller(['A']).should.equal('Give me an A!\n');
+    name.cheerYeller(['B']).should.equal('Give me a B!\n');
+    name.cheerYeller(['C']).should.equal('Give me a C!\n');
+    name.cheerYeller(['D']).should.equal('Give me a D!\n');
+    name.cheerYeller(['E']).should.equal('Give me an E!\n');
+    name.cheerYeller(['F']).should.equal('Give me an F!\n');
+    name.cheerYeller(['G']).should.equal('Give me a G!\n');
+    name.cheerYeller(['H']).should.equal('Give me an H!\n');
+    name.cheerYeller(['I']).should.equal('Give me an I!\n');
+    name.cheerYeller(['O']).should.equal('Give me an O!\n');
+    name.cheerYeller(['U']).should.equal('Give me a U!\n');
+    name.cheerYeller(['J', 'K']).should.equal('Give me a J!\nGive me a K!\n');
+    name.cheerYeller(['L', 'P']).should.equal('Give me an L!\nGive me a P!\n');
+
+  })
+})
+
+
+describe('CLI', function () {
+  it('should print something to the terminal', function (done) {
+    cp.execFile("./app.js", ["ca t"], function (err, stdout) {
+      stdout.should.equal('Give me a C!\nGive me an A!\nGive me a T!\n')
       done();
     })
   })
 })
+
+
+//=================================
+
+//INTEGRATION TESTS ---------------
+// describe('CLI', function () {
+//   it('should thank me for downloading', function (done) {
+//     console.log('childprocess');
+//     cp.execFile('./app.js', function (err, stdout) {
+//       console.log('err', err);
+//       console.log('stdout', stdout);
+//       stdout.should.equal('Thanks for downloading my app!\n')
+//       done();
+//     })
+//   })
+// })
 
 
 
@@ -22,66 +74,66 @@ describe.only('CLI', function () {
 //UNIT TESTS -----============---------==========
 
 
-describe('Animal', function () {
-  describe('constructor', function () {
-    it('should return an animal object', function () {
-      var animal = new Animal();
+// describe('Animal', function () {
+//   describe('constructor', function () {
+//     it('should return an animal object', function () {
+//       var animal = new Animal();
 
-      animal.should.be.an('object');
-      animal.should.be.an.instanceOf(Animal)
-    })
+//       animal.should.be.an('object');
+//       animal.should.be.an.instanceOf(Animal)
+//     })
 
-    it('should be alive', function () {
-      var animal = new Animal();
-      animal.alive.should.be.true;
-    })
-    it('should have 100% health', function () {
-      var animal = new Animal()
-      animal.health.should.equal(1);
-    })
-    it('should accept a type', function () {
-      var cat = new Animal('cat');
-      var dog = new Animal('dog');
+//     it('should be alive', function () {
+//       var animal = new Animal();
+//       animal.alive.should.be.true;
+//     })
+//     it('should have 100% health', function () {
+//       var animal = new Animal()
+//       animal.health.should.equal(1);
+//     })
+//     it('should accept a type', function () {
+//       var cat = new Animal('cat');
+//       var dog = new Animal('dog');
 
-      cat.species.should.equal('cat');
-      dog.species.should.equal('dog');
-    });
+//       cat.species.should.equal('cat');
+//       dog.species.should.equal('dog');
+//     });
 
-    describe('#updateHealthStats()', function () {
-      it('should change the health', function (done) {
-        var animal = new Animal();
-        var health = animal.health;
+//     describe('#updateHealthStats()', function () {
+//       it('should change the health', function (done) {
+//         var animal = new Animal();
+//         var health = animal.health;
 
-        animal.updateHealthStats(function () {
-          animal.health.should.not.equal(1);
-          done();
-        });
+//         animal.updateHealthStats(function () {
+//           animal.health.should.not.equal(1);
+//           done();
+//         });
 
-      })
-
-
-    })
-
-    describe('#beCute()', function () {
-      it('should be a prototype method', function () {
-        var animal = new Animal();
-        animal.should.respondTo('beCute');
-        animal.should.not.have.ownProperty('beCute');
-      })
-      it('should make the animal cute', function () {
-        var animal = new Animal();
-        animal.isCute.should.not.be.true;
-        animal.beCute();
-        animal.isCute.should.be.true;
-      })
-    })
-
-  });
+//       })
 
 
+//     })
+
+//     describe('#beCute()', function () {
+//       it('should be a prototype method', function () {
+//         var animal = new Animal();
+//         animal.should.respondTo('beCute');
+//         animal.should.not.have.ownProperty('beCute');
+//       })
+//       it('should make the animal cute', function () {
+//         var animal = new Animal();
+//         animal.isCute.should.not.be.true;
+//         animal.beCute();
+//         animal.isCute.should.be.true;
+//       })
+//     })
+
+//   });
 
 
-});
+
+
+// });
 
 describe('Tests', function() {
   it('truthyness', function () {
